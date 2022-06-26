f08fd1421770840b1797ea3970dbf5cc7e73b01d
Irvan Taufik <irvantaufik28@gmail.com>
2022-06-26 10:46:54 +0700

    mengubah angkot ke 5 menjadi lembur

--- bab-javascripts/latihan-perulangan/angkot3.js
+++ bab-javascripts/latihan-perulangan/angkot3.js
@@ -2,9 +2,9 @@ let jmlAngkot =10;
 let angkotBeroprasi = 6;
 
 for (let i =1; i<=jmlAngkot; i++){
-    if(i <= angkotBeroprasi ) 
+    if(i <= angkotBeroprasi && i !==5) 
     { console.log('angkot ke - ' + i + ' sedang Beroprasi');
-    } else if (i=== 8 || i === 10){
+    } else if (i=== 8 || i === 10 || i===5){
         console.log('angkot ke - ' + i + ' sedang lembur')
     } else
      {console.log ('angkot ke - ' + i + ' sedang Ruksak');}
