e7cd67516944523debca9561c132bcacce00095b
Irvan Taufik <irvantaufik28@gmail.com>
2022-06-26 09:02:53 +0700

    menabahkan file htlm & js Latihan Angkot 2

---
+++ bab-javascripts/latihan-perulangan/angkot2.js
@@ -0,0 +1,10 @@
+let jmlAgkot = 10;
+let angkotBeroprasi=6;
+
+for (let i=1; i<=jmlAgkot; i++){
+    if ( i <= angkotBeroprasi){
+         console.log ('Angkot ke '+ i + ' Berajalan dengan Baik');
+    } else 
+        {console.log ('Angkot ke '+ i + ' Sedang Ruksak');
+} 
+}
