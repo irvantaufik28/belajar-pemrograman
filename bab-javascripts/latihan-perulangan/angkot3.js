61a6ddf814b5fd6f3498b67ceda39b890e3a3679
Irvan Taufik <irvantaufik28@gmail.com>
2022-06-26 10:27:03 +0700

    mengubah angkot 8 & 10 menjadi lembur

--- bab-javascripts/latihan-perulangan/angkot3.js
+++ bab-javascripts/latihan-perulangan/angkot3.js
@@ -1,11 +1,10 @@
 let jmlAngkot =10;
 let angkotBeroprasi = 6;
-let angkotLembur = 8;
 
 for (let i =1; i<=jmlAngkot; i++){
     if(i <= angkotBeroprasi ) 
     { console.log('angkot ke - ' + i + ' sedang Beroprasi');
-    } else if (i===angkotLembur){
+    } else if (i=== 8 || i === 10){
         console.log('angkot ke - ' + i + ' sedang lembur')
     } else
      {console.log ('angkot ke - ' + i + ' sedang Ruksak');}
