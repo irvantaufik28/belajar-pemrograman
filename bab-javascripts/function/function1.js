3e7a2f1c33dfc533e516337bc9d36b6b99afd69a
irvan taufik <irvantaufik28@gmail.com>
2022-06-26 20:24:55 +0700

    tambah materi

---
+++ bab-javascripts/function/function1.js
@@ -0,0 +1,16 @@
+// function Declaration
+
+function penambahan (a,b){
+    return a+b
+}
+console.log(penambahan(5,5));
+
+
+
+
+
+// function Expression
+// let penambahan = function (a ,b){
+// return a+b
+// }
+// console.log(penambahan(5,6));
