135f37e71212e28619fe841a8d2aaaf38b25ab26
Irvan Taufik <irvantaufik28@gmail.com>
2022-06-26 12:47:48 +0700

    menambhakan folder quis dicoding

---
+++ bab-javascripts/quis-coding-dicoding/quis1.js
@@ -0,0 +1,37 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+
+let firstName = 'Irvan';
+let lastName = 'Taufik';
+let age = 28;
+isMarried = true;
+
+console.log (`${firstName} ${lastName} ${age} ${isMarried}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Jangan hapus kode di bawah ini
+ */
+//  module.exports = {
+//     firstName, lastName, age, isMarried,
+//   };
