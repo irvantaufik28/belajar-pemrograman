87d908ec5ab2389278f9e5987f70fbc8888b2f7a
irvan taufik <irvantaufik28@gmail.com>
2022-06-27 23:28:06 +0700

    portofolio

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary:'#dc2626',
+        drak:'#991b1b',
+      },
+    },
+  },
+  plugins: [],
+};
